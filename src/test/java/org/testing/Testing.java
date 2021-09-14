a4d60da25a059386305978113b6370660c892612
Gowtham019 <dgowtham526.gd@gmail.com>
2021-09-14 20:56:36 +0530

    Initial push

---
+++ src/test/java/org/testing/Testing.java
@@ -0,0 +1,38 @@
+package org.testing;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Testing {
+public static void main(String[] args) throws IOException {
+	
+	File file = new File("C:\\Users\\Gowtham\\eclipse-workspace\\Maven1\\Excel\\MavenExc.xlsx");
+	FileInputStream stream = new FileInputStream(file);
+	
+	Workbook workbook = new XSSFWorkbook(stream);
+	Sheet sheet = workbook.getSheet("Datas");
+	
+	for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+		
+	Row row = sheet.getRow(i);
+	for (int j = 0; j < row.getPhysicalNumberOfCells(); j++) {
+		Cell cell = row.getCell(j);
+		System.out.println(cell);
+	}
+		
+	}
+	
+	
+	
+	
+	
+}
+}
